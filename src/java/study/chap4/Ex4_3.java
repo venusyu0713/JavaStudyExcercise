1da723b6932a2ec3f2d9c79ceca69be5932ce5e0
Venus Yu <venusyu0713@gmail.com>
2015-09-09 14:33:11 +0900

    package 이름 수정

--- src/java/study/chap4/Ex4_3.java
+++
@@ -1,5 +0,0 @@
-package java.study.chap4;
-
-public class Ex4_3 {
-
-}
---
+++ src/study/java/chap4/Ex4_3.java
@@ -0,0 +1,25 @@
+package study.java.chap4;
+
+public class Ex4_3 {
+	public static void main(String args[])
+	{
+		int sum = 0;
+		int count = 1;
+		while(count <=10)
+		{
+			System.out.print(count +": ");
+			for(int i=1; i<=count ; i++)
+			{
+				sum += i;
+				if(i!=count)
+					System.out.print(i + "+");
+				else if (i==count)
+					System.out.print(i);
+			}
+			System.out.println();
+			count++;
+		}
+		
+		System.out.println("sum: " + sum);
+	}
+}
